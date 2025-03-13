398dc0a1cff3aa6a01f862142fc310b139bc716c
timofeykasimtsev <timofey.kasimtsev@conciso.de>
2025-03-13 13:49:51 +0100

    LC-001: refactor LengthOfSubstringWithNoDuplicatesh

---
+++ src/LengthOfSubstringWithNoDuplicates.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class LengthOfSubstringWithNoDuplicates {
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        HashMap<Character, Integer> characters = new HashMap<>();
+        int result = 0;
+
+        ArrayList<Integer> listOfResult = new ArrayList();
+
+        for (int i = 0; i < s.length(); i++) {
+            result++;
+            if (!characters.containsKey(s.charAt(i))) {
+                characters.put(s.charAt(i), result);
+                listOfResult.add(result);
+
+            } else {
+                int k = characters.get(s.charAt(i));
+
+                result  -= k;
+
+
+                for (Character character : characters.keySet()) {
+                    characters.put(character, (int) characters.get(character) - k);
+                }
+
+                characters.put(s.charAt(i), result);
+                characters.entrySet().removeIf(entry -> entry.getValue() <= 0);
+                listOfResult.add(result);
+            }
+        }
+
+        int max = Collections.max(listOfResult);
+
+        return max;
+
+    }
+
+    public static void main(String[] args) {
+        String test = "aabaab!bb";
+        int result  = lengthOfLongestSubstring(test);
+        System.out.println(result);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -7,48 +7,6 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        String test = "aabaab!bb";
-        int result  = lengthOfLongestSubstring(test);
-        System.out.println(result);
-
-    }
-
-    public static int lengthOfLongestSubstring(String s) {
-
-        HashMap<Character, Integer> characters = new HashMap<>();
-        int result = 0;
-
-        ArrayList<Integer> listOfResult = new ArrayList();
-
-        for (int i = 0; i < s.length(); i++) {
-            result++;
-            if (!characters.containsKey(s.charAt(i))) {
-                characters.put(s.charAt(i), result);
-                listOfResult.add(result);
-
-            } else {
-                int k = characters.get(s.charAt(i));
-
-                result  -= k;
-
-
-                for (Character character : characters.keySet()) {
-                    characters.put(character, (int) characters.get(character) - k);
-                }
-
-
-                characters.put(s.charAt(i), result);
-
-                characters.entrySet().removeIf(entry -> entry.getValue() <= 0);
-                listOfResult.add(result);
-
-            }
-        }
-
-        int max = Collections.max(listOfResult);
-
-        return max;
-
     }
 
     public static int[] twoSum(int[] nums, int target){
