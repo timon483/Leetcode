e09a1931b0a842e1f998e06f624f8f7de4c9c121
timofeykasimtsev <timofey.kasimtsev@conciso.de>
2025-03-13 13:47:30 +0100

    LC-001: refactor ListNode

--- src/ListNode.java
+++ src/ListNode.java
@@ -6,4 +6,79 @@ public class ListNode {
      ListNode(int val) { this.val = val; }
      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
+     public static ListNode addTwo(ListNode l1, ListNode l2) {
+
+
+          ListNode result = sumTwoNodes(l1, l2, 0);
+
+          return result;
+
+     }
+
+     public static ListNode sumTwoNodes(ListNode l1, ListNode l2, int rest) {
+
+          if (l1.next != null && l2.next != null) {
+               int sum = l1.val + l2.val + rest;
+               return new ListNode(valueWithoutRest(sum), sumTwoNodes(l1.next, l2.next, isRest(sum)));
+          }
+
+          if (l1.next == null && l2.next != null) {
+               int sum = l1.val + l2.val + rest;
+               return new ListNode(valueWithoutRest(sum), sumOneNode(l2.next, isRest(sum)));
+          } else if (l1.next != null && l2.next == null) {
+               int sum = l1.val + l2.val + rest;
+               return new ListNode(valueWithoutRest(sum), sumOneNode(l1.next, isRest(sum)));
+
+          }
+
+          int sum = l1.val + l2.val + rest;
+          if (sum < 10) {
+               return new ListNode(sum);
+          } else {
+               return new ListNode(valueWithoutRest(sum), new ListNode(sum/10));
+          }
+
+     }
+
+     public static ListNode sumOneNode(ListNode listNode, int rest) {
+          int sum = listNode.val + rest;
+          if (listNode.next != null) {
+               sum = listNode.val + rest;
+               return new ListNode(valueWithoutRest(sum), sumOneNode(listNode.next, isRest(sum)));
+          } else {
+               if (sum < 10) {
+                    return new ListNode(sum);
+               } else {
+                    return new ListNode(valueWithoutRest(sum), new ListNode(sum/10));
+               }
+          }
+     }
+
+     public static int isRest(int sum) {
+          if (sum >= 10) {
+               return 1;
+          }
+          return 0;
+     }
+
+     public static int valueWithoutRest(int sum) {
+          if (sum >= 10) {
+               return sum - 10;
+          }
+          return sum;
+     }
+
+     public static void main(String[]  args) {
+          ListNode soloListNode = new ListNode(1);
+
+          ListNode listNode1 = new ListNode(1);
+          ListNode listNode2 = new ListNode(9, listNode1);
+          ListNode listNode3 = new ListNode(9, listNode2);
+
+          ListNode test = addTwo(soloListNode, listNode3);
+
+          System.out.println(test);
+     }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -7,36 +7,6 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-
-
-        ListNode soloListNode = new ListNode(1);
-
-        ListNode listNode1 = new ListNode(1);
-        ListNode listNode2 = new ListNode(9, listNode1);
-        ListNode listNode3 = new ListNode(9, listNode2);
-/*        ListNode listNode4 = new ListNode(9, listNode3);
-        ListNode listNode5 = new ListNode(9, listNode4);
-        ListNode listNode6 = new ListNode(9, listNode5);
-        ListNode listNode7 = new ListNode(9, listNode6);
-        ListNode listNode8 = new ListNode(9, listNode7);
-        ListNode listNode9 = new ListNode(9, listNode8);
-        ListNode listNode10 = new ListNode(1, listNode9);*/
-
-
-
-
-
-/*        ListNode listNode2 = new ListNode(4, listNode3);
-        ListNode listNode1 = new ListNode(2, listNode2);
-
-        ListNode listNode4 = new ListNode(4);
-        ListNode listNode6 = new ListNode(6, listNode4);
-        ListNode listNode5 = new ListNode(5, listNode6);*/
-
-      //  ListNode test = addTwo(soloListNode, listNode3);
-
-       // System.out.println(test);
-
         String test = "aabaab!bb";
         int result  = lengthOfLongestSubstring(test);
         System.out.println(result);
@@ -79,70 +49,6 @@ public class Main {
 
         return max;
 
-    }
-
-    public static ListNode addTwo(ListNode l1, ListNode l2) {
-
-
-        ListNode result = sumTwoNodes(l1, l2, 0);
-
-        return result;
-
-    }
-
-
-
-    public static ListNode sumTwoNodes(ListNode l1, ListNode l2, int rest) {
-
-        if (l1.next != null && l2.next != null) {
-            int sum = l1.val + l2.val + rest;
-            return new ListNode(valueWithoutRest(sum), sumTwoNodes(l1.next, l2.next, isRest(sum)));
-        }
-
-        if (l1.next == null && l2.next != null) {
-            int sum = l1.val + l2.val + rest;
-            return new ListNode(valueWithoutRest(sum), sumOneNode(l2.next, isRest(sum)));
-        } else if (l1.next != null && l2.next == null) {
-            int sum = l1.val + l2.val + rest;
-            return new ListNode(valueWithoutRest(sum), sumOneNode(l1.next, isRest(sum)));
-
-        }
-
-        int sum = l1.val + l2.val + rest;
-        if (sum < 10) {
-            return new ListNode(sum);
-        } else {
-            return new ListNode(valueWithoutRest(sum), new ListNode(sum/10));
-        }
-
-    }
-
-    public static ListNode sumOneNode(ListNode listNode, int rest) {
-        int sum = listNode.val + rest;
-        if (listNode.next != null) {
-            sum = listNode.val + rest;
-            return new ListNode(valueWithoutRest(sum), sumOneNode(listNode.next, isRest(sum)));
-        } else {
-            if (sum < 10) {
-                return new ListNode(sum);
-            } else {
-                return new ListNode(valueWithoutRest(sum), new ListNode(sum/10));
-            }
-        }
-    }
-
-    public static int isRest(int sum) {
-        if (sum >= 10) {
-            return 1;
-        }
-        return 0;
-    }
-
-    public static int valueWithoutRest(int sum) {
-        if (sum >= 10) {
-            return sum - 10;
-        }
-        return sum;
     }
 
     public static int[] twoSum(int[] nums, int target){
