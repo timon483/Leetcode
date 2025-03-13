e23f65a184db31baeb41ac2c0cb2d18847a34335
timofeykasimtsev <timofey.kasimtsev@conciso.de>
2025-03-13 15:04:00 +0100

    LC-005: palindrom

--- src/LongestPalindrom.java
+++ src/LongestPalindrom.java
@@ -2,4 +2,22 @@
  * Given a string s, return the longest palindromic substring in s.
  */
 public class LongestPalindrom {
+
+    public static String longestPalindrome(String s) {
+
+        //TODO
+
+        return "";
+
+    }
+
+    public static void main(String[] args){
+        String test = "babab";
+        String result = longestPalindrome(test);
+        System.out.println(result);
+
+
+
+    }
+
 }
